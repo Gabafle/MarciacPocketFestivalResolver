e1b5a21e39d069eb235ef22242732f5f249d1d3b
Gabafle <gabriel.peter@hotmail.fr>
2025-06-15 19:04:12 +0100

    Create struct Party and Modifie Element for Print and equal (for Testing with Test U)

--- structure/Enceinte.py
+++ structure/Enceinte.py
@@ -3,3 +3,14 @@ class Enceinte:
         self.score = score
         self.width = width
         self.height = height
+
+
+    def __repr__(self):  # ou __str__
+        return f"Enceinte(score='{self.score}', width={self.width}, height={self.height})"
+
+    def __eq__(self, other):
+        if not isinstance(other, Enceinte):
+            return NotImplemented
+        return (self.score == other.score and
+                self.width == other.width and
+                self.height == other.height)
