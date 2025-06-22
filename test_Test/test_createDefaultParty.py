f68c41bec3c869e2c3a49cbe38a3ce96b193ecc2
Gabafle <gabriel.peter@hotmail.fr>
2025-06-22 10:12:42 +0100

    ADD Create Default Party

--- test_Test/test_createDefaultParty.py
+++ test_Test/test_createDefaultParty.py
@@ -1,4 +1,41 @@
+from DefaultParty import createDefaultParty
+from structure.Chaise import Chaise
+from structure.Distributeur import Distributeur
+from structure.Enceinte import Enceinte
+from structure.Fontaine import Fontaine
+from structure.Party import Party
+from structure.Rampe import Rampe
+from structure.Scene import Scene
+from structure.Stand import Stand
+from structure.Tente import Tente
+from structure.Toilette import Toilette
+from structure.Vigile import Vigile
 
 
 
-def test_createDefaultPartyWith():
+'''
+[Vigile(), Vigile(), Vigile(), Chaise(), Chaise(), Chaise(), Chaise(), Chaise(), Distributeur(),
+                     Enceinte(), Enceinte(), Fontaine(), Rampe(), Scene(), Stand(), Stand(), Stand(), Tente(), Tente(),
+                     Toilette(), Toilette()]
+'''
+
+def test_createDefaultParty():
+    # Arrange
+    expectedParty = Party()
+
+    expectedParty.scene = Scene()
+    expectedParty.rampe = Rampe()
+    expectedParty.listVigile = [Vigile(), Vigile(), Vigile()]
+    expectedParty.listChaise = [Chaise(), Chaise(), Chaise(), Chaise(), Chaise()]
+    expectedParty.listDistributeur = [Distributeur()]
+    expectedParty.listEnceinte = [Enceinte(),Enceinte()]
+    expectedParty.listTente = [Tente(), Tente()]
+    expectedParty.listToilette = [Toilette(), Toilette()]
+    expectedParty.listFontaine = [Fontaine()]
+    expectedParty.listStand = [Stand(), Stand(), Stand()]
+
+    # Act
+    actualParty = createDefaultParty()
+
+    # Assert
+    assert actualParty == expectedParty
